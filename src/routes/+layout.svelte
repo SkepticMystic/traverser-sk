18465ee32810ff5cea017b5a95e8c173782c91c0
SkepticMystic <rossk29@gmail.com>
2024-03-01 21:10:59 +0200

    @vercel/analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
+	import { onMount } from 'svelte';
 	import '../app.css';
+	import { inject } from '@vercel/analytics';
+
+	onMount(() => {
+		inject();
+	});
 </script>
 
 <div class="mx-auto max-w-4xl my-3 px-3">
