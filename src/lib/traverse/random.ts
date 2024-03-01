e0d59bca10334f85e836e5d2c82ecc81ac7d5a9f
SkepticMystic <rossk29@gmail.com>
2024-03-01 19:50:41 +0200

    fix: random.n shouldn't allow 1

--- src/lib/traverse/random.ts
+++ src/lib/traverse/random.ts
@@ -3,6 +3,6 @@ import { range } from '$lib/utils/array';
 const int = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 export const random = {
-	n: () => int(1, 25),
+	n: () => int(5, 25),
 	steps: (n?: number) => range(int(1, 4)).map(() => int(0, n ? n - 1 : 20))
 };
